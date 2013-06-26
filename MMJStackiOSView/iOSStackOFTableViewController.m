de3f849eb69dfca4c72195111d21123f4c4dd978
Mihaela Mihaljevic Jakic <mihaelamj@me.com>
2013-06-26 18:24:01 +0200

    added refreshControl and multithreaded upload of questions

--- MMJStackiOSView/iOSStackOFTableViewController.m
+++ MMJStackiOSView/iOSStackOFTableViewController.m
@@ -18,7 +18,28 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    self.soQuestions = [MMJStackOverflowFetcher latestiOSQuestions];
+    [self loadLatestiOSSOQuestions];
+    //set the target action in code for refreshControl, because it cannot be done in XCode
+	[self.refreshControl addTarget:self
+                            action:@selector(loadLatestiOSSOQuestions)
+                  forControlEvents:(UIControlEventValueChanged)];
+}
+
+- (void)loadLatestiOSSOQuestions
+{
+    [self.refreshControl beginRefreshing];
+    dispatch_queue_t loadSO= dispatch_queue_create("MMJStackOverflowFetcher latest questions", NULL);
+    dispatch_async(loadSO, ^{
+        //simulate long operation
+        [NSThread sleepForTimeInterval:2.0];
+        // make local variable to stay out of the UI
+        NSArray *latestiOSQuestions = [MMJStackOverflowFetcher latestiOSQuestions];
+        dispatch_async(dispatch_get_main_queue(), ^{
+            // in the main queue set the self.photos because it affects the UI
+            self.soQuestions = latestiOSQuestions;
+            [self.refreshControl endRefreshing];
+        });
+    });
 }
 
 
